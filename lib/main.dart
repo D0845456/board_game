8d9d6fab8b2025b7c5016c6ff8d12933b479398a
D0845456 <ysyang860122@gmail.com>
2022-10-20 00:54:52 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: const MyHomePage(title: 'Board Game Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // @override
+  // Widget build(BuildContext context) {
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     title: Text(widget.title),
+    //   ),
+    //   body: Center(
+    //     child: Column(
+    //       mainAxisAlignment: MainAxisAlignment.center,
+    //       children: <Widget>[
+    //         const Text(
+    //           'You have pushed the button this many times:',
+    //         ),
+    //         Text(
+    //           '$_counter',
+    //           style: Theme.of(context).textTheme.headline4,
+    //         ),
+    //       ],
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.add),
+    //   ),
+    // );
+    @override
+    Widget build(BuildContext context) {
+      return DefaultTabController(
+        length: tabList.length,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("舊書你了"),
+            leading: const Icon(FontAwesomeIcons.book),
+            actions: const <Widget>[
+              IconButton(
+                icon: Icon(FontAwesomeIcons.search),
+                onPressed: null,
+              )
+            ],
+            bottom: TabBar(
+              tabs: tabList.map((choice) {
+                return Tab(
+                  text: choice.title,
+                  icon: Icon(choice.icon),
+                );
+              }).toList(),
+            ),
+          ),
+          body: TabBarView(
+              children: tabList.map((choice) {
+                return Center(
+                  child: Icon(
+                    choice.icon,
+                    size: 100.0,
+                  ),
+                );
+              }).toList()),
+        ),
+      );
+    }
+
+}
+class TabChoice {
+  final String title;
+  final IconData icon;
+  const TabChoice(this.title, this.icon);
+}
+
+const List<TabChoice> tabList = <TabChoice>[
+  TabChoice('地圖', Icons.map_rounded),
+  TabChoice('知識點', Icons.book),
+];
